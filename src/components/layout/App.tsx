f72a5a4516b924c745e6e8d2af1268a75857d3a3
Antarux <theAntarux@proton.me>
2026-01-21 23:32:03 +0100

    Refactor layout, remove Header, update styles
    > Removed the Header component and its usage from App.tsx. > Enhanced Footer with new styling and updated text.
    > Expanded index.css with custom CSS variables and utility classes for improved theming and layout consistency.

--- src/components/layout/App.tsx
+++ src/components/layout/App.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="grow">
+      <main className="grow p-4">
         <Outlet />
       </main>
       <Footer />
--- src/components/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-        <header>
-            <p>Header</p>
-        </header>
-    );
-}
-
-export default Header;
